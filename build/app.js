59d0b946aad405ff7c81a70ad02584376ef19cf7
Soham Phargade <soham.g.phargade@gmail.com>
2025-10-15 22:32:35 -0500

    add basic auth

---
+++ build/app.js
@@ -0,0 +1,27 @@
+documend.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+    console.log(app)
+});
+
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+            document.write(`Hello ${user.displayName}`);
+            console.log(user)
+        })
+        .catch(console.log)
+}
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+            //document.write(`Hello ${user.displayName}`);
+            document.getElementById('greeting').innerText = `Hello ${user.displayName}`;
+            console.log(user);
+        })
+        .catch(console.log);
+}
