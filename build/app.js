abcf13d24d57312e2f62251f54fa5ae83f154d0c
Soham Phargade <soham.g.phargade@gmail.com>
2025-10-16 13:16:03 -0500

    chore

--- build/app.js
+++ build/app.js
@@ -9,16 +9,6 @@ function googleLogin() {
     firebase.auth().signInWithPopup(provider)
         .then(result => {
             const user = result.user;
-            document.write(`Hello ${user.displayName}`);
-            console.log(user)
-        })
-        .catch(console.log)
-}
-function googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-        .then(result => {
-            const user = result.user;
             //document.write(`Hello ${user.displayName}`);
             document.getElementById('greeting').innerText = `Hello ${user.displayName}`;
             console.log(user);
